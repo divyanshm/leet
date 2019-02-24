e32b4f9f8caf5411c2bac091e3fab94f713db5b0
Divyansh Manchanda <divman@microsoft.com>
2019-02-24 15:10:40 +0530

    more solutions

---
+++ Leetcode/LinkedList/AddLists.cs
@@ -0,0 +1,47 @@
+﻿namespace Leetcode.LinkedList
+{
+    // Q2, Medium
+    class AddLists
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = null, tail = null;
+            int carry = 0;
+
+            while (true)
+            {
+                if (l1 == null && l2 == null && carry == 0)
+                {
+                    break;
+                }
+
+                var num1 = l1?.val ?? 0;
+                var num2 = l2?.val ?? 0;
+
+                var sum = num1 + num2 + carry;
+                carry = sum / 10;
+
+                if (carry == 1)
+                {
+                    sum = sum - 10;
+                }
+
+                if (result == null)
+                {
+                    result = new ListNode(sum);
+                    tail = result;
+                }
+                else
+                {
+                    tail.next = new ListNode(sum);
+                    tail = tail.next;
+                }
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/Strings/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Strings
+{
+    // Q3, Medium
+    class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var set = new HashSet<char>();
+            var longestLength = 0;
+            var currentWindowLength = 0;
+
+            int length = s.Length;
+            int windowStart = 0, windowEnd = 0;
+            while (windowStart < length && windowEnd < length)
+            {
+                // current window does not contain this character
+                if (!set.Contains(s[windowEnd]))
+                {
+                    set.Add(s[windowEnd]);
+                    windowEnd++;
+
+                    currentWindowLength++;
+                    if (currentWindowLength > longestLength)
+                    {
+                        longestLength = currentWindowLength;
+                    }
+                }
+                else
+                {
+                    // Whoops, encountered an element which was already in the sliding window
+                    // start reducing size of sliding window
+                    set.Remove(s[windowStart]);
+                    windowStart++;
+                    currentWindowLength--;
+                }
+            }
+
+            return longestLength;
+        }
+    }
+}
