9513c3b9a691e0a1eebd3289af8b0674868c0d9a
Divyansh Manchanda <divman@microsoft.com>
2019-02-24 12:39:06 +0530

    two sum problem

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Leetcode.LinkedList;
+using Leetcode.Random;
 using Leetcode.Strings;
 using Leetcode.Trees;
 
@@ -17,7 +18,7 @@ namespace Leetcode
             //var node6 = new ListNode(6) {next = node7};
             //var node5 = new ListNode(5) {next = node6};
             //var node4 = new ListNode(4) { next = null };
-            var node3 = new ListNode(3) { next = null };
+            /*var node3 = new ListNode(3) { next = null };
             var node2 = new ListNode(2) { next = node3 };
             var node1 = new ListNode(1) { next = node2 };
             // new ReorderListProblem().ReorderList(node1);
@@ -26,7 +27,7 @@ namespace Leetcode
             var node3a = new ListNode(3) { next = node4a };
             var node1a = new ListNode(1) { next = node3a };
 
-            new MergedListsProblem().MergeTwoLists(node1, node1a);
+            new MergedListsProblem().MergeTwoLists(node1, node1a);*/
 
             /*int[][] two = new int[8][];
             two[0] = new[] {1, 1, 0, 0, 0, 0, 0, 0};
@@ -53,6 +54,17 @@ namespace Leetcode
             var lca = new LeastCommonAncestor().LowestCommonAncestor(three, four, seven);*/
 
             //var isIncluded = new PermutationInStringProblem().CheckInclusion("adc", "dcda");
+
+            var nums = new int[]
+            {
+                230, 863, 916, 585, 981, 404, 316, 785, 88, 12, 70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 325, 422,
+                815, 650, 920, 125, 277, 336, 221, 847, 168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 863, 997, 794,
+                587, 124, 321, 212, 957, 764, 173, 314, 422, 927, 783, 930, 282, 306, 506, 44, 926, 691, 568, 68, 730,
+                933, 737, 531, 180, 414, 751, 28, 546, 60, 371, 493, 370, 527, 387, 43, 541, 13, 457, 328, 227, 652,
+                365, 430, 803, 59, 858, 538, 427, 583, 368, 375, 173, 809, 896, 370, 789
+            };
+
+            var sol = new TwoSumProblem().TwoSum(nums.ToArray(), 9);
         }
     }
 }
---
+++ Leetcode/Random/TwoSumProblem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.Random
+{
+    // Easy. Q1.
+    class TwoSumProblem
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var answer = new int[2];
+            bool isAnswerFound = false;
+
+            var lookup = new Dictionary<int, int>();
+            for (int i=0; i<nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if (lookup.ContainsKey(diff))
+                {
+                    isAnswerFound = true;
+                    answer[0] = lookup[diff];
+                    answer[1] = i;
+                    break;
+                }
+
+                if (!lookup.ContainsKey(nums[i]))
+                {
+                    lookup.Add(nums[i], i);
+                }
+            }
+
+            if (isAnswerFound)
+            {
+                return answer;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
