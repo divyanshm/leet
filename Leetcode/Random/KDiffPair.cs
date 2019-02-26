127e0cf63e3cd034a5ac6a47c07eab65780dd9d0
Divyansh Manchanda <divyanshm@gmail.com>
2019-02-26 07:51:33 +0530

    more problems

---
+++ Leetcode/Random/KDiffPair.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Leetcode.Random
+{
+    // Q.532, Easy
+    class KDiffPair
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+            if(nums == null)
+            {
+                return 0;
+            }
+
+            // Stupid test case
+            if (k < 0)
+            {
+                return 0;
+            }
+
+            var encountered = new HashSet<int>();
+            var registered = new HashSet<int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var currentNumber = nums[i];
+                if(encountered.Contains(currentNumber + k))
+                {
+                    if(!registered.Contains(currentNumber + currentNumber + k))
+                    {
+                        registered.Add(currentNumber + currentNumber + k);
+                    }
+                }
+                else if(encountered.Contains(currentNumber - k))
+                {
+                    if (!registered.Contains(currentNumber + currentNumber - k))
+                    {
+                        registered.Add(currentNumber + currentNumber - k);
+                    }
+                }
+
+                if(!encountered.Contains(currentNumber))
+                {
+                    encountered.Add(currentNumber);
+                }
+            }
+
+            return registered.Count;
+        }
+    }
+}
