75cd3987dc1fd5180c5b969e67d85ba3cbbc3b6d
Divyansh Manchanda <divyanshm@gmail.com>
2019-02-26 06:52:50 +0530

    checkpoint all activity on laptop

---
+++ Leetcode/LinkedList/RemoveListValue.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode.LinkedList
+{
+    public class RemoveListValue
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode prev = null, current = head, newHead = head;
+            if(head == null)
+            {
+                return null;
+            }
+
+            while(current != null)
+            {
+                if(current.val == val)
+                {
+                    if(prev != null)
+                    {
+                        prev.next = current.next;
+                    }
+                    else
+                    {
+                        newHead = current.next;
+                    }
+                }
+                else
+                {
+                    prev = current;
+                }
+
+                current = current.next;
+            }
+
+            return newHead;
+        }
+    }
+}
