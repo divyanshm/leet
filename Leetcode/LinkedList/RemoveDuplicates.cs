36e4444a2a00413926dcdc86c23bde7c53be6f49
Divyansh Manchanda <divyanshm@gmail.com>
2019-06-10 20:04:56 +0530

    remove duplicates

---
+++ Leetcode/LinkedList/RemoveDuplicates.cs
@@ -0,0 +1,33 @@
+﻿namespace Leetcode.LinkedList
+{
+    // Q.83, Easy
+    public class RemoveDuplicates
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if(head == null)
+            {
+                return null;
+            }
+
+            ListNode trav1 = head, trav2 = head.next;
+            while(trav2 != null)
+            {
+                if(trav1.val == trav2.val)
+                {
+                    var nextNode = trav2.next;
+                    trav1.next = nextNode;
+                    trav2.next = null;
+                    trav2 = nextNode;
+                }
+                else
+                {
+                    trav1 = trav2;
+                    trav2 = trav2.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
